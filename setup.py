6a410ed453c541b38a802b35471a93ca632e2dd4
Manoj Pillay <spadejac@gmail.com>
2018-04-22 12:39:56 -0700

    Added setup.py and examples

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name="CoordinateTranslator",
+    version='0.1',
+    author='Manoj Pillay',
+    author_email='spadejac@gmail.com',
+    py_modules=['CoordinateTranslator', 'Cigar', 'CigarTests'],
+    install_requires=[
+        'Click',
+    ],
+    entry_points='''
+        [console_scripts]
+        CoordinateTranslator=CoordinateTranslator:cli
+    ''',
+    project_urls={
+                 	'Source': 'https://gitlab.com/spadejac/interview_assignments/tree/master/U2FsdGVkX19c3IfNjlyyxNyniQpXP.Yp'
+		 }
+)
