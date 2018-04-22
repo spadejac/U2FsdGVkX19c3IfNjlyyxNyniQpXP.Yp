9309bd03de52cb8d0da4149d0ef309b83b01d7ce
Manoj Pillay <spadejac@gmail.com>
2018-04-22 12:38:06 -0700

    Improved help,

--- CoordinateTranslator.py
+++ CoordinateTranslator.py
@@ -38,25 +38,30 @@ class CoordinateTranslator(object):
             for ref in self.transcripts[tr_name]:
                 cigars = self.transcripts[tr_name][ref]
                 for cigar in cigars:
-                    outfile.write( '\t'.join([tr_name, zero_pos, ref, 
-                                              str(cigar.map(int(zero_pos)))
-                                              ]) + '\n' )
+                    ref_coord = cigar.map(int(zero_pos))
+                    if ref_coord:
+                        outfile.write( '\t'.join([tr_name, zero_pos, ref, 
+                                                  str(ref_coord)
+                                                  ]) + '\n' )
                     
         print "Available mapping has been written into {}".format(outfile.name)
+
 
 @click.option('--transcripts_file', required=True, type=click.Path(exists=True),
               help='Transcripts file (4 column tab-separated)')
 @click.option('--query_file', required=True, type=click.Path(exists=True),
               help='Query file (2 column tab-separated')
-@click.option('--output_file', required=True, type=click.File(mode='w'))
+@click.option('--output_file', required=True, type=click.File(mode='w'),
+              help='Name for file to write results into')
 @click.command(context_settings=dict(
     ignore_unknown_options=True,
-    help_option_names=[],
+    help_option_names=[ '-h', '--help'],
 ))
 def cli(transcripts_file, query_file, output_file):
     '''
-    Command line interface with options
+    A translator for zero-based transcript coordinates to reference coordinates
     '''
+    
     ct = CoordinateTranslator(transcripts_file, query_file)
     ct.process_transcripts_file()
     ct.translate(output_file)
