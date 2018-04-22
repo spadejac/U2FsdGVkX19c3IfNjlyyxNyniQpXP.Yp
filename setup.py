ae797522895210ffca237024987da4c7d7b06b7b
Manoj Pillay <spadejac@gmail.com>
2018-04-22 16:03:53 -0700

    Added version details to setup

--- setup.py
+++ setup.py
@@ -15,5 +15,18 @@ setup(
     ''',
     project_urls={
                  	'Source': 'https://gitlab.com/spadejac/U2FsdGVkX19c3IfNjlyyxNyniQpXP.Yp'
-		 }
+		 },
+    classifiers=[
+    'Development Status :: 3 - Alpha',
+
+    # Indicate who your project is intended for
+    'Intended Audience :: Developers',
+
+    # Pick your license as you wish (should match "license" above)
+     'License :: OSI Approved :: MIT License',
+
+    # Specify the Python versions you support here. In particular, ensure
+    # that you indicate whether you support Python 2, Python 3 or both.
+    'Programming Language :: Python :: 2.7',
+   ],
 )
